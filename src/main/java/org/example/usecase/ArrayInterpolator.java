49fb0cabb839bb83e68b191334215b0e8b837299
Juan Rossi <juan.lucas.rossi@gmail.com>
2023-09-26 21:04:48 -0300

    feat: implementa tratativa para quando é fornecido dois arrays de tamanhos diferentes

--- src/main/java/org/example/usecase/ArrayInterpolator.java
+++ src/main/java/org/example/usecase/ArrayInterpolator.java
@@ -5,6 +5,10 @@ import org.example.protocols.Interpolator;
 public class ArrayInterpolator {
     @Override
     public int[] interpolateArray(int[] array1, int[] array2) {
-        // TODO: Implementar lógica de interpolação
+        if (array1.length != array2.length) {
+            throw new IllegalArgumentException("Os arrays devem ter o mesmo tamanho para interpolação.");
+        }
+
+        return []
     }
 }
