210cc7b3e3603be5d9a6b17747e489bef923666c
Juan Rossi <juan.lucas.rossi@gmail.com>
2023-09-26 21:01:57 -0300

    test: cria e realiza o setup da classe de teste do ArrayInterpolator

---
+++ src/test/java/usecase/ArrayInterpolatorTest.java
@@ -0,0 +1,17 @@
+package usecase;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ArrayInterpolatorTest {
+
+    private ArrayInterpolator arrayInterpolator;
+
+   @Before
+    public void setUp() {
+        arrayInterpolator = new ArrayInterpolator();
+    }
+}
