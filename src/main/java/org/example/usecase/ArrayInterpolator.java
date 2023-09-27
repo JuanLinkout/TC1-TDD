540104b536d9e1a4e6bf944d7af50eb13d22d8ad
Juan Rossi <juan.lucas.rossi@gmail.com>
2023-09-26 21:02:24 -0300

    feat: inicializar classe ArrayInterpolate

---
+++ src/main/java/org/example/usecase/ArrayInterpolator.java
@@ -0,0 +1,10 @@
+package org.example.usecase;
+
+import org.example.protocols.Interpolator;
+
+public class ArrayInterpolator {
+    @Override
+    public int[] interpolateArray(int[] array1, int[] array2) {
+        // TODO: Implementar lógica de interpolação
+    }
+}
